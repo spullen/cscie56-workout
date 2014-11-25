b9ce13df720f851a62f44ccb48fe5b153a9d35c5
Scott Pullen <s.pullen05@gmail.com>
2014-11-24 22:34:45 -0500

    adding s2 overrides for auth and registration

---
+++ grails-app/controllers/com/workout/RegisterController.groovy
@@ -0,0 +1,83 @@
+package com.workout
+
+import grails.plugin.springsecurity.SpringSecurityUtils
+import grails.plugin.springsecurity.authentication.dao.NullSaltSource
+import grails.plugin.springsecurity.ui.RegistrationCode
+
+class RegisterController extends grails.plugin.springsecurity.ui.RegisterController {
+    def index() {
+        def copy = [:] + (flash.chainedParams ?: [:])
+        copy.remove 'controller'
+        copy.remove 'action'
+        [command: new RegisterCommand(copy)]
+    }
+
+    def register(RegisterCommand command) {
+
+        if (command.hasErrors()) {
+            render view: 'index', model: [command: command]
+            return
+        }
+
+        String salt = saltSource instanceof NullSaltSource ? null : command.username
+        def user = lookupUserClass().newInstance(
+                email: command.email,
+                username: command.username,
+                firstName: command.firstName,
+                lastName: command.lastName,
+                phoneNumber: command.phoneNumber,
+                accountLocked: false,
+                enabled: true
+        )
+
+        RegistrationCode registrationCode = springSecurityUiService.register(user, command.password, salt)
+        if (registrationCode == null || registrationCode.hasErrors()) {
+            // null means problem creating the user
+            flash.error = message(code: 'spring.security.ui.register.miscError')
+            flash.chainedParams = params
+            redirect action: 'index'
+            return
+        }
+
+        flash.message = 'Account created! Please log in.'
+
+        redirect controller: 'login', action: 'index'
+    }
+}
+
+class RegisterCommand {
+    String username
+    String email
+    String firstName
+    String lastName
+    String phoneNumber
+    String password
+    String password2
+
+    def grailsApplication
+
+    static constraints = {
+        username blank: false, validator: { value, command ->
+            if (value) {
+                def User = command.grailsApplication.getDomainClass(
+                        SpringSecurityUtils.securityConfig.userLookup.userDomainClassName).clazz
+                if (User.findByUsername(value)) {
+                    return 'registerCommand.username.unique'
+                }
+            }
+        }
+        email blank: false, email: true, validator: { value, command ->
+            if (value) {
+                def User = command.grailsApplication.getDomainClass(
+                        SpringSecurityUtils.securityConfig.userLookup.userDomainClassName).clazz
+                if (User.findByEmail(value)) {
+                    return 'registerCommand.email.unique'
+                }
+            }
+        }
+        password blank: false, validator: RegisterController.passwordValidator
+        password2 validator: RegisterController.password2Validator
+        firstName blank: false
+        lastName blank: false
+    }
+}
