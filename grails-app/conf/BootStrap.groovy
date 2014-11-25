ef0ab7da771c3a4825039b763dc8207549a3dfa9
Scott Pullen <s.pullen05@gmail.com>
2014-11-24 22:23:52 -0500

    adding a test user to bootstrap file

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,16 @@
+import com.workout.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        User user = new User(
+                username: 'me',
+                password: 'password',
+                firstName: 'Test',
+                lastName: 'User',
+                email: 'testUser@test.com'
+        )
+        user.save(flush: true)
     }
     def destroy = {
     }
