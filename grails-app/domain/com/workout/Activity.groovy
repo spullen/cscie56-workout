4dfd7ebf0cf0f2a4a7524ff6e8079cc03927cdaf
Scott Pullen <s.pullen05@gmail.com>
2014-11-24 20:20:49 -0500

    adding activity domain class

---
+++ grails-app/domain/com/workout/Activity.groovy
@@ -0,0 +1,47 @@
+package com.workout
+
+class Activity {
+    ActivityType activityType = ActivityType.RUNNING
+    BigDecimal amount
+    MetricType metric = MetricType.MILES
+    Date start
+    Date end
+    BigDecimal duration
+    String notes
+
+    static constraints = {
+        activityType blank: false
+        amount blank: false, min: 0.1
+        metric blank: false
+        start blank: false
+        end blank: false
+        duration nullable: true
+        notes nullable: true, maxSize: 2000
+    }
+}
+
+public enum ActivityType {
+    RUNNING('Running'),
+    CYCLING('Cycling'),
+    WALKING('Walking'),
+    PUSH_UPS('Push Ups'),
+    SIT_UPS('Sit Ups')
+
+    String id
+
+    ActivityType(String id) {
+        this.id = id
+    }
+}
+
+public enum MetricType {
+    MILES('Miles'),
+    REPS('Reps'),
+    COUNT('Count')
+
+    String id
+
+    MetricType(String id) {
+        this.id = id
+    }
+}
