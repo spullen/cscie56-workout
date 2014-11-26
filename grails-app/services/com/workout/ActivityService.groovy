1c1fb060facc2e9f3cd3ffeddd3ddce5d4009440
Scott Pullen <s.pullen05@gmail.com>
2014-11-25 23:26:12 -0500

    adding some notes on how to do integration

--- grails-app/services/com/workout/ActivityService.groovy
+++ grails-app/services/com/workout/ActivityService.groovy
@@ -8,10 +8,22 @@ class ActivityService {
 
     SpringSecurityService springSecurityService
 
-    def create(Activity activityInstance) {
-        activityInstance.user = springSecurityService.currentUser
-        activityInstance.save()
+    def create(Activity activity) {
+        activity.user = springSecurityService.currentUser
+        activity.save()
 
         // TODO: goal integration
+        // Find active goal for user, activity type, and metric
+        // Update current amount value
+        // Decide if goal has been completed
+    }
+
+    def update(Activity activity) {
+        activity.save()
+
+        // TODO: goal integration
+        // Find active goal for user, activity type, and metric
+        // Re-calculate current amount and update
+        // Decide if goal has been completed (this could be mean going from completed to incomplete if adjustments go under target)
     }
 }
