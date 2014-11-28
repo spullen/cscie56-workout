59cff1adf8f9f37ec7940c77af8ab03699b61dce
Scott Pullen <s.pullen05@gmail.com>
2014-11-27 23:36:25 -0500

    fixing datasource for prod

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -30,18 +30,16 @@ environments {
         }
     }
     production {
-        production {
-            dataSource {
-                dbCreate = "update"
-                driverClassName = "org.postgresql.Driver"
-                dialect = org.hibernate.dialect.PostgreSQLDialect
+        dataSource {
+            dbCreate = "update"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
 
-                uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
 
-                url = "jdbc:postgresql://"+uri.host+uri.path
-                username = uri.userInfo.split(":")[0]
-                password = uri.userInfo.split(":")[1]
-            }
+            url = "jdbc:postgresql://"+uri.host+uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
         }
     }
 }
