e8ed633951c1f42deb9b633e4a4272c6603064b5
Scott Pullen <s.pullen05@gmail.com>
2014-11-27 22:26:18 -0500

    adding test for determining the goal's accomplished state

--- test/unit/com/workout/GoalSpec.groovy
+++ test/unit/com/workout/GoalSpec.groovy
@@ -15,6 +15,43 @@ class GoalSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "determineAccomplishedState"() {
+        given:
+        Goal goal = new Goal(
+                user: [id: 1] as User,
+                title: '5K Training',
+                activityType: ActivityType.RUNNING,
+                metric: MetricType.DISTANCE,
+                targetAmount: 15,
+                targetDate: (new Date()).clearTime().next()
+        )
+
+        when: 'currentAmount is less than the targetAmount'
+        goal.currentAmount = 10
+        goal.determineAccomplishedState()
+
+        then: 'accomplished should be false and dateAccomplished should be null'
+        !goal.accomplished
+        goal.dateAccomplished == null
+
+        when: 'accomplished is true and dateAccomplished is set and currentAmount is less than the target'
+        goal.accomplished = true
+        goal.dateAccomplished = new Date()
+        goal.currentAmount = 10
+        goal.determineAccomplishedState()
+
+        then: 'accomplished should be false and dateAccomplished should be null'
+        !goal.accomplished
+        goal.dateAccomplished == null
+
+        when: 'accomplished is false and dateAccomplished is null and currentAmount is over the target amount'
+        goal.accomplished = false
+        goal.dateAccomplished = null
+        goal.currentAmount = 16
+        goal.determineAccomplishedState()
+
+        then: 'accomplished should be true and dateAccomplished should be set'
+        goal.accomplished
+        goal.dateAccomplished != null
     }
 }
