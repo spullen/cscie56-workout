43042a48ec556715e0016df411eeac09faad6d7c
Scott Pullen <s.pullen05@gmail.com>
2014-11-28 15:31:26 -0500

    adding methods to DashboardService to find various things

--- grails-app/services/com/workout/DashboardService.groovy
+++ grails-app/services/com/workout/DashboardService.groovy
@@ -8,6 +8,20 @@ class DashboardService {
 
     SpringSecurityService springSecurityService
 
+    List<Goal> activeGoals() {
+        User user = springSecurityService.loadCurrentUser()
+        Goal.findAllByUserAndAccomplished(user, false, [sort: 'targetDate', order: 'desc'])
+    }
 
+    List<Goal> recentlyAccomplishedGoals() {
+        User user = springSecurityService.loadCurrentUser()
+        Date start = new Date().clearTime() - 7
+        Date end = new Date().clearTime() + 1
+        Goal.findAllByUserAndDateAccomplishedBetween(user, start, end, [sort: 'targetDate', order: 'desc'])
+    }
 
+    List<Activity> recentlyCreatedActivities() {
+        User user = springSecurityService.loadCurrentUser()
+        Activity.findAllByUser(user, [sort: 'dateCreated', order: 'desc', max: 25])
+    }
 }
